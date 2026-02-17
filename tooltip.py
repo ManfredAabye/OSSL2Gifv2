eb6aa3e479e9c5d4864cb57555a31c7f75e4adef
Manfred Aabye <zainhofer@gmx.de>
2026-02-17 12:15:18 +0100

    170220261214

--- tooltip.py
+++ tooltip.py
@@ -22,9 +22,14 @@ class ToolTip:
     def show_tip(self, event=None):
         if self.tipwindow or not self.text:
             return
-        x, y, cx, cy = self.widget.bbox("insert") if hasattr(self.widget, 'bbox') else (0,0,0,0)
-        x = x + self.widget.winfo_rootx() + 25
-        y = y + self.widget.winfo_rooty() + 20
+        # Position 25 Pixel über dem Mauszeiger
+        if event is not None:
+            x = event.x_root + 0
+            y = event.y_root - 50
+        else:
+            x, y, cx, cy = self.widget.bbox("insert") if hasattr(self.widget, 'bbox') else (0,0,0,0)
+            x = x + self.widget.winfo_rootx() + 25
+            y = y + self.widget.winfo_rooty() - 25
         self.tipwindow = tw = tk.Toplevel(self.widget)
         tw.wm_overrideredirect(True)
         tw.wm_geometry(f"+{x}+{y}")
